e68294709e3e2a9057b40ff3bade9fc542fc46a4
NIKHIL DESAI <68286207+NIKHIL130@users.noreply.github.com>
2021-01-28 18:02:56 +0530

    onclick event

---
+++ img.js
@@ -0,0 +1,12 @@
+var img = document.getElementById("img1");
+
+function on()
+{
+      img.src='https://previews.123rf.com/images/murika/murika1511/murika151100069/48123160-bright-glowing-incandescent-light-bulb-on-a-white-background.jpg'
+          
+}
+function off()
+{
+    img.src='https://image.shutterstock.com/image-illustration/3d-rendering-clear-light-bulb-260nw-699554389.jpg'
+
+}
